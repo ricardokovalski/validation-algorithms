ca390f8b6fdbccddb8b672f1a7ad61600eeceb1a
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2019-05-10 10:16:39 -0300

    Update algorithm validation.

--- cnpj.php
+++ cnpj.php
@@ -1,8 +1,6 @@
 <?php
 
-
-$cnpj = preg_replace("/[^0-9]/", "", $cnpj);
-$cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+$cnpj = str_pad(preg_replace("/[^0-9]/", "", $cnpj), 14, '0', STR_PAD_LEFT);
 	
 if (strlen($cnpj) != 14) {
     return false;
@@ -12,30 +10,26 @@ if (preg_match('/^(\d)\1{13}$/', $cnpj) {
     return false;
 }
 	
-		$j = 5;
-		$k = 6;
-		$soma1 = "";
-		$soma2 = "";
+$j = 5;
+$k = 6;
+$soma1 = "";
+$soma2 = "";
 
-		for ($i = 0; $i < 13; $i++) {
+for ($i = 0; $i < 13; $i++) {
+    $j = $j == 1 ? 9 : $j;
+    $k = $k == 1 ? 9 : $k;
 
-			$j = $j == 1 ? 9 : $j;
-			$k = $k == 1 ? 9 : $k;
+    $soma2 += ($cnpj{$i} * $k);
 
-			$soma2 += ($cnpj{$i} * $k);
+    if ($i < 12) {
+        $soma1 += ($cnpj{$i} * $j);
+    }
 
-			if ($i < 12) {
-				$soma1 += ($cnpj{$i} * $j);
-			}
+    $k--;
+    $j--;
+}
 
-			$k--;
-			$j--;
+$digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
+$digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
 
-		}
-
-		$digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
-		$digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
-
-		return (($cnpj{12} == $digito1) and ($cnpj{13} == $digito2));
-	 
-	}
+return (($cnpj{12} == $digito1) and ($cnpj{13} == $digito2));
