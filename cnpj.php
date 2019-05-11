b6efce6eb7d56271fbfb56f9e516b0f57ac523fb
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2019-05-11 14:16:01 -0300

    Refactoring algorithm.

--- cnpj.php
+++ cnpj.php
@@ -9,20 +9,22 @@ if (strlen($cnpj) != 14) {
 if (preg_match('/^(\d)\1{13}$/', $cnpj)) {
     return false;
 }
-	
-$soma1 = 0;
-$soma2 = 0;
 
-for ($i = 0, $j = 5, $k = 6; $i < 13; $i++, $j--, $k--) {
-    $j = $j == 1 ? 9 : $j;
-    $k = $k == 1 ? 9 : $k;
+for ($i = 0, $j = 5, $k = 6, $soma1 = 0, $soma2 = 0; $i < 13; $i++, $j--, $k--) {
+    $j = ($j == 1) ? 9 : $j;
+    $k = ($k == 1) ? 9 : $k;
     $soma2 += ($cnpj{$i} * $k);
     if ($i < 12) {
         $soma1 += ($cnpj{$i} * $j);
     }
 }
 
-$digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
-$digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
+if ($cnpj{12} != (($soma1 % 11) < 2) ? 0 : (11 - ($soma1 % 11))) {
+    return false;
+}
 
-return (($cnpj{12} == $digito1) and ($cnpj{13} == $digito2));
+if ($cnpj{13} != (($soma2 % 11) < 2) ? 0 : (11 - ($soma2 % 11))) {
+    return false;
+}
+
+return true;
